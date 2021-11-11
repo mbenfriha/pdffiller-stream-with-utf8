ca34c17d94517eb93db02dc5203ef8e473e2010a
mohamed.benfriha@epitech.eu <mohamed.benfriha@epitech.eu@epitech.eu>
2021-11-11 01:43:23 +0100

    add CP1252 encode

--- fdf.js
+++ fdf.js
@@ -1,29 +1,23 @@
-var fs = require('fs');
-var _ = require('lodash');
+var fs = require("fs");
+var _ = require("lodash");
 
 // only this sequence in FDF header requires char codes
 var headerChars = new Buffer(
-    (String.fromCharCode(226)) +
-    (String.fromCharCode(227)) +
-    (String.fromCharCode(207)) +
-    (String.fromCharCode(211)) +
+  String.fromCharCode(226) +
+    String.fromCharCode(227) +
+    String.fromCharCode(207) +
+    String.fromCharCode(211) +
     "\n"
 );
 
 var header = Buffer.concat([
-    new Buffer("%FDF-1.2\n"),
-    headerChars,
-    new Buffer(
-        "1 0 obj \n" +
-        "<<\n" +
-        "/FDF \n" +
-        "<<\n" +
-        "/Fields [\n"
-    )
+  new Buffer("%FDF-1.2\n"),
+  headerChars,
+  new Buffer("1 0 obj \n" + "<<\n" + "/FDF \n" + "<<\n" + "/Fields [\n"),
 ]);
 
 var footer = new Buffer(
-    "]\n" +
+  "]\n" +
     ">>\n" +
     ">>\n" +
     "endobj \n" +
@@ -33,40 +27,40 @@ var footer = new Buffer(
     "/Root 1 0 R\n" +
     ">>\n" +
     "%%EOF\n"
-)
+);
 
 var escapeString = function escapeString(value) {
-    var out = value.toString();
-    out = out.replace(/\\/g, "\\\\");
-    out = out.replace(/\(/g, "\\(");
-    out = out.replace(/\)/g, "\\)");
-    out = out.toString("utf8");
-    return out;
-}
+  var out = value.toString();
+  out = out.replace(/\\/g, "\\\\");
+  out = out.replace(/\(/g, "\\(");
+  out = out.replace(/\)/g, "\\)");
+  out = out.toString("utf8");
+  return out;
+};
 
-exports.createFdf = function (data) {    
-    var body = new Buffer([]);
+exports.createFdf = function (data) {
+  var body = new Buffer([]);
 
-    _.mapKeys(data, function (value, name) {
-        try {
-            body = Buffer.concat([
-                body,
-                new Buffer(
-                    "<<\n" +
-                    "/T (" +
-                    escapeString(name) +
-                    ")\n" +
-                    "/V (" +
-                    escapeString(value) +
-                    ")\n" +
-                    ">>\n"
-                )
-            ]);
-        } catch (err) {
-            let errMsg = "Cannot escape string: '" + name + ": " + value + "'.";
-            throw Error(errMsg);
-        }
-    });
+  _.mapKeys(data, function (value, name) {
+    try {
+      body = Buffer.concat([
+        body,
+        new Buffer(
+          "<<\n" +
+            "/T (" +
+            escapeString(name) +
+            ")\n" +
+            "/V (" +
+            escapeString(value) +
+            ")\n" +
+            ">>\n"
+        ),
+      ]);
+    } catch (err) {
+      let errMsg = "Cannot escape string: '" + name + ": " + value + "'.";
+      throw Error(errMsg);
+    }
+  });
 
-    return Buffer.concat([header, body, footer]);
-}
+  return Buffer.concat([header, body, footer]);
+};
